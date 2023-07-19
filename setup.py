455e899b161f59805dd18952ef3132a89baee964
Guillermo Vizcaíno Román <guillevzn@gmail.com>
2023-07-19 16:26:35 +0100

    Summary

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Easy to use Fred API with Python.'
 
 # Setting up
@@ -18,7 +18,7 @@ setup(
     author_email="<guillevzn@gmail.com>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=['pandas', 'requests', 'json'],
+    install_requires=['pandas'],
     keywords=['python', 'fred', 'database', 'API' ],
     classifiers=[
         "Development Status :: 1 - Planning",
